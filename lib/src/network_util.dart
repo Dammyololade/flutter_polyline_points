f1e0c250ddfee95928ce53e53a1ec2810d3e491b
dammy_ololade <damola@kobo360.com>
2020-02-01 11:06:52 +0300

    merge pull request

--- lib/src/network_util.dart
+++ lib/src/network_util.dart
@@ -1,53 +1,52 @@
 part of flutter_polyline_points;
 
-enum TravelMode {
-  driving,
-  bicycling,
-  transit,
-  walking
-}
+enum TravelMode { driving, bicycling, transit, walking }
 
-class NetworkUtil
-{
-
+class NetworkUtil {
   ///Get the encoded string from google directions api
   ///
-  Future<List<PointLatLng>> getRouteBetweenCoordinates(String googleApiKey, double originLat, double originLong,
-      double destLat, double destLong, TravelMode travelMode) async
-  {
+  Future<List<PointLatLng>> getRouteBetweenCoordinates(
+      String googleApiKey,
+      double originLat,
+      double originLong,
+      double destLat,
+      double destLong,
+      TravelMode travelMode) async {
     String mode = travelMode.toString().replaceAll('TravelMode.', '');
     List<PointLatLng> polylinePoints = [];
-    String url = "https://maps.googleapis.com/maps/api/directions/json?origin=" +
-        originLat.toString() +
-        "," +
-        originLong.toString() +
-        "&destination=" +
-        destLat.toString() +
-        "," +
-        destLong.toString() +
-        "&mode=$mode" +
-        "&key=$googleApiKey";
+    String url =
+        "https://maps.googleapis.com/maps/api/directions/json?origin=" +
+            originLat.toString() +
+            "," +
+            originLong.toString() +
+            "&destination=" +
+            destLat.toString() +
+            "," +
+            destLong.toString() +
+            "&mode=$mode" +
+            "&key=$googleApiKey";
     print('GOOGLE MAPS URL: ' + url);
     var response = await http.get(url);
     try {
       if (response?.statusCode == 200) {
-        polylinePoints = decodeEncodedPolyline(json.decode(
-            response.body)["routes"][0]["overview_polyline"]["points"]);
+        var parsedJson = json.decode(response.body);
+        if(parsedJson["routes"] != null && parsedJson["routes"].isNotEmpty){
+          polylinePoints = decodeEncodedPolyline(parsedJson["routes"][0]["overview_polyline"]["points"]);
+        } else {
+          throw Exception(parsedJson["error_message"]);
+        }
       }
     } catch (error) {
       throw Exception(error.toString());
     }
-    print(polylinePoints);
     return polylinePoints;
   }
-
 
   ///decode the google encoded string using Encoded Polyline Algorithm Format
   /// for more info about the algorithm check https://developers.google.com/maps/documentation/utilities/polylinealgorithm
   ///
   ///return [List]
-  List<PointLatLng> decodeEncodedPolyline(String encoded)
-  {
+  List<PointLatLng> decodeEncodedPolyline(String encoded) {
     List<PointLatLng> poly = [];
     int index = 0, len = encoded.length;
     int lat = 0, lng = 0;
@@ -71,7 +70,8 @@ class NetworkUtil
       } while (b >= 0x20);
       int dlng = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
       lng += dlng;
-      PointLatLng p = new PointLatLng((lat / 1E5).toDouble(), (lng / 1E5).toDouble());
+      PointLatLng p =
+          new PointLatLng((lat / 1E5).toDouble(), (lng / 1E5).toDouble());
       poly.add(p);
     }
     return poly;
