b13d1919bea8adf41e4ec2a4ac60960bf6378366
Andrei Cosma <andrei.cosma@teta.so>
2022-08-08 16:00:15 +0300

    fix(added path render fix)

--- lib/src/network_util.dart
+++ lib/src/network_util.dart
@@ -50,6 +50,7 @@ class NetworkUtil {
     //String url = uri.toString();
     // print('GOOGLE MAPS URL: ' + url);
     Dio dio = Dio();
+    print("Hello :)");
     var response = await dio.get(uri.toString(),
         options: Options(headers: {
           "Access-Control-Allow-Origin": "*",
@@ -83,29 +84,43 @@ class NetworkUtil {
     List<PointLatLng> poly = [];
     int index = 0, len = encoded.length;
     int lat = 0, lng = 0;
+    BigInt Big0 = BigInt.from(0);
+    BigInt Big0x1f = BigInt.from(0x1f);
+    BigInt Big0x20 = BigInt.from(0x20);
 
     while (index < len) {
-      int b, shift = 0, result = 0;
+      int shift = 0;
+      BigInt b, result;
+      result = Big0;
       do {
-        b = encoded.codeUnitAt(index++) - 63;
-        result |= (b & 0x1f) << shift;
+        b = BigInt.from(encoded.codeUnitAt(index++) - 63);
+        result |= (b & Big0x1f) << shift;
         shift += 5;
-      } while (b >= 0x20);
-      int dlat = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
-      lat += dlat;
+      } while (b >= Big0x20);
+      BigInt rShifted = result >> 1;
+      int dLat;
+      if (result.isOdd)
+        dLat = (~rShifted).toInt();
+      else
+        dLat = rShifted.toInt();
+      lat += dLat;
 
       shift = 0;
-      result = 0;
+      result = Big0;
       do {
-        b = encoded.codeUnitAt(index++) - 63;
-        result |= (b & 0x1f) << shift;
+        b = BigInt.from(encoded.codeUnitAt(index++) - 63);
+        result |= (b & Big0x1f) << shift;
         shift += 5;
-      } while (b >= 0x20);
-      int dlng = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
-      lng += dlng;
-      PointLatLng p =
-          new PointLatLng((lat / 1E5).toDouble(), (lng / 1E5).toDouble());
-      poly.add(p);
+      } while (b >= Big0x20);
+      rShifted = result >> 1;
+      int dLng;
+      if (result.isOdd)
+        dLng = (~rShifted).toInt();
+      else
+        dLng = rShifted.toInt();
+      lng += dLng;
+
+      poly.add(PointLatLng((lat / 1E5).toDouble(), (lng / 1E5).toDouble()));
     }
     return poly;
   }
